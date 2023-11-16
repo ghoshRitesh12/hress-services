f01c9c66207236dfc3f7bc5efd7bcd7981b3e310
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-11-17 00:18:45 +0530

    feat: add `/payout/pdf` route

--- src/routes/payout.js
+++ src/routes/payout.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { handlePayout } from "../controllers/payout.controller.js";
+import { handlePayoutPDFGen } from "../controllers/payout-pdf.controller.js";
 
 const router = Router();
 
-router.post('/', handlePayout)
+router.post("/", handlePayout);
+router.post("/pdf", handlePayoutPDFGen);
 
 export default router;
