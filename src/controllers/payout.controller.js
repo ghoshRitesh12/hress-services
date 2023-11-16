b19f42621c630b129cadd3802578ac68c9db72a4
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-11-17 00:18:13 +0530

    perf: update payout generation method

--- src/controllers/payout.controller.js
+++ src/controllers/payout.controller.js
@@ -1,22 +1,8 @@
+import "../services/payout.js";
 import createHttpError from "http-errors";
-import { isReqVerified } from "../helpers/verifyReq.js";
-import { invokePayoutGeneration } from "../services/payout.js";
 
 export async function handlePayout(req, res, next) {
   try {
-    const parentDomain = req?.headers?.["x-parent-service-domain"];
-    if (!parentDomain || parentDomain !== process.env?.PARENT_SERVICE_DOMAIN) {
-      throw createHttpError.BadRequest();
-    }
-
-    const serviceKey = decodeURIComponent(req?.query?.key);
-    if (!serviceKey) {
-      throw createHttpError.BadRequest("Service key required");
-    }
-    if (!(await isReqVerified(serviceKey))) {
-      throw createHttpError.Forbidden("Unauthorized request 🙄");
-    }
-
     const dateRegex = /^\d{1,2}-\d{1,2}-\d{4}$/;
 
     const incomePeriod = req?.body?.incomePeriod;
@@ -24,7 +10,9 @@ export async function handlePayout(req, res, next) {
       throw createHttpError.BadRequest("Invalid Payload");
     }
 
-    invokePayoutGeneration(incomePeriod);
+    // invokePayoutGeneration(incomePeriod)
+    process.emit("gen:payout", incomePeriod);
+
     return res.json({
       msg: "Generating payout. Hang tight!"
     });
